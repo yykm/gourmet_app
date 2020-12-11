127a070e0da5cac1d5d925e90e7bf59198dfa3a0
yykm <okametuu@gmail.com>
2020-12-11 11:33:00 +0900

    feat/Auth導入

--- back_end/routes/web.php
+++ back_end/routes/web.php
@@ -11,5 +11,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::post('/search', 'SearchController@search');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
