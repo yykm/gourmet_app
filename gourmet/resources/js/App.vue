c1725add6d1e6652ab3c1ccd3fdaad1566399609
yykm <okametuu@gmail.com>
2020-12-16 05:53:34 +0900

    Laravel vue.js scaffold

--- gourmet/resources/js/App.vue
+++ gourmet/resources/js/App.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">App</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        APP_I'm an example component.
                     </div>
                 </div>
             </div>
---
+++ gourmet/resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
